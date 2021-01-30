95feaebd7e4961d3b4cc4ab9cffa231810477ed7
Paula Gómez <paulagomez@MacBook-Air-de-Paula.local>
2021-01-30 08:48:35 -0500

    modificacion en test

--- api.calculator.test.js
+++ api.calculator.test.js
@@ -3,45 +3,24 @@ const request=require("supertest");
 const app = require("./app");
 const { response } = require("express");
 
-describe("Testing /add path",()=>{
-
-    test("it should return status code 200",done=>{
+describe("testing /add path", () => {
+    test("it should return status code 200", done => {
         request(app)
-        .get("/app?a=1&b=1")
-        .then(response=>{
+        .get("/add?a=1&b=1")
+        .then(response => {
             expect(response.statusCode).toBe(200);
             done();
-            
         });
-
     });
 
-    //prueba
-
-
-    test("it should return application/json content-type",done=>{
+    test("it should return application/json content-type", done => {
         request(app)
-        .get("/app?a=1&b=1")
-        .then(response=>{
+        .get("/add?a=1&b=1")
+        .then(response => {
             expect(response.type).toBe("application/json");
             done();
-            
         });
-
     });
-
-    test("it should return valid object ",done=>{
-        request(app)
-        .get("/app?a=1&b=1")
-        .then(response=>{
-            expect(response.body).not.toBeNull();
-            expect(response.body.result).not.toBeUndefine();
-            done();
-            
-        });
-
-    });
-
 
     test("it should return a valid json objetc ", done => {
         request(app)
@@ -53,5 +32,12 @@ describe("Testing /add path",()=>{
         });
     });
 
-
+    test("it should return a correct answer", done => {
+        request(app)
+        .get("/add?a=1&b=4")
+        .then(response => {
+            expect(response.body.result).toBe(5);
+            done();
+        });
+    });
 })
